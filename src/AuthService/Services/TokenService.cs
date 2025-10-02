290bec56771981a4ae382137c1360eacc7b3cf25
kutluaylav <kutluay3344@hotmail.com>
2025-10-02 14:46:35 +0300

    feat:keycloak created

---
+++ src/AuthService/Services/TokenService.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthService.Services
+{
+    public class TokenService
+    {
+        
+    }
+}
